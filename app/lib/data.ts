ced21194966cd346b9a39cb76b3f2b13d47d7a44
Felipe Ramírez <zelzazor.ramirez62@gmail.com>
2023-12-29 11:23:35 -0400

    feature: image and font optimization

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -4,9 +4,10 @@ import {
   CustomersTableType,
   InvoiceForm,
   InvoicesTable,
+  LatestInvoice,
   LatestInvoiceRaw,
-  User,
   Revenue,
+  User,
 } from './definitions';
 import { formatCurrency } from './utils';
 
@@ -32,7 +33,7 @@ export async function fetchRevenue() {
   }
 }
 
-export async function fetchLatestInvoices() {
+export async function fetchLatestInvoices(): Promise<LatestInvoice[]> {
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
